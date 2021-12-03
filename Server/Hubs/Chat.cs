518c87b4c0b23d5cffc3051db0e6004abcef1c29
Dragan Pepic <nedimco2006@gmail.com>
2021-12-03 17:31:08 +0100

    Add project files.

---
+++ Server/Hubs/Chat.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRRR.Shared;
+
+namespace SignalRRR.Server.Hubs
+{
+    public class Chat : Hub
+    {
+        public void Test(Poruka p)
+        {
+            p.Vreme = DateTime.Now; 
+            Console.WriteLine($"{p.Vreme.TimeOfDay} -- {p.Ime}: {p.Tekst}");
+            Clients.All.SendAsync("dodaj", p);
+        }
+    }
+}
---
+++ Shared/Poruka.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRRR.Shared
+{
+    public class Poruka
+    {
+        public string Tekst { get; set; }
+        public string Ime { get; set; }
+        public DateTime Vreme { get; set; }
+    }
+}
